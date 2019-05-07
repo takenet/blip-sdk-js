ccdea1e15f0ac7ec780c2c868203864fea949bfe
Henrique Torquato <henriquet@take.net>
2019-05-07 15:41:49 -0300

    Added extension base tests

---
+++ test/blipSdkClientExtensionBaseTest.js
@@ -0,0 +1,109 @@
+'use strict';
+
+import ExtensionImplementation from './helpers/ExtensionImplementation';
+
+const TEST_URI = '/test';
+const TEST_URI_TEMPLATE = '/test/{0}'
+const TEST_TO = 'postmaster@test.ai';
+const TEST_TYPE = 'application/vnd.iris.extension.test+json';
+const TEST_RESOURCE = {
+    name: 'test'
+};
+
+require('chai').should();
+
+describe('ExtensionBase', function() {
+
+    beforeEach((done) => {
+        this.extension = new ExtensionImplementation();
+        done();
+    });
+
+    it('should create GET command', (done) => {
+
+        const command = this.extension
+            .createGetCommand(TEST_URI, TEST_TO);
+
+        command.uri.should.equal(TEST_URI);
+        command.to.should.equal(TEST_TO);
+
+        command.should.have.property('id');
+        command.should.not.have.property('resource');
+        command.method.should.equal('get');
+
+        done();
+
+    });
+
+    it('should create DELETE command', (done) => {
+
+        const command = this.extension
+            .createDeleteCommand(TEST_URI, TEST_TO);
+
+        command.uri.should.equal(TEST_URI);
+        command.to.should.equal(TEST_TO);
+
+        command.should.have.property('id');
+        command.should.not.have.property('resource');
+        command.method.should.equal('delete');
+
+        done();
+
+    });
+
+    it('should create SET command', (done) => {
+
+        const command = this.extension
+            .createSetCommand(TEST_URI, TEST_TYPE, TEST_RESOURCE, TEST_TO);
+
+        command.uri.should.equal(TEST_URI);
+        command.to.should.equal(TEST_TO);
+        command.type.should.equal(TEST_TYPE);
+        command.resource.should.equal(TEST_RESOURCE);
+
+        command.should.have.property('id');
+        command.should.have.property('resource');
+        command.method.should.equal('set');
+
+        done();
+
+    });
+
+    it('should create MERGE command', (done) => {
+
+        const command = this.extension
+            .createMergeCommand(TEST_URI, TEST_TYPE, TEST_RESOURCE, TEST_TO);
+
+        command.uri.should.equal(TEST_URI);
+        command.to.should.equal(TEST_TO);
+        command.type.should.equal(TEST_TYPE);
+        command.resource.should.equal(TEST_RESOURCE);
+
+        command.should.have.property('id');
+        command.should.have.property('resource');
+        command.method.should.equal('merge');
+
+        done();
+
+    });
+
+    it('should build uri path', (done) => {
+        const parameter = (Math.random() * 100).toFixed(0);
+        const uri = this.extension.buildUri(TEST_URI_TEMPLATE, parameter);
+        uri.should.equal(`${TEST_URI}/${parameter}`);
+        done();
+    });
+
+    it('should build uri resource query', (done) => {
+        const query = {
+            skip: 0,
+            take: 100,
+            deep: true
+        };
+
+        const uri = this.extension.buildResourceQuery(TEST_URI, query);
+        uri.should.equal(`${TEST_URI}?skip=0&take=100&deep=true`)
+        done();
+    });
+
+});
---
+++ test/helpers/ExtensionImplementation.js
@@ -0,0 +1,33 @@
+import ExtensionBase from "../../src/Extensions/ExtensionBase";
+
+export default class ExtensionImplementation extends ExtensionBase {
+    
+    createGetCommand(uri, to) {
+        return this._createGetCommand(uri, to);
+    }
+
+    createSetCommand(uri, type, resource, to) {
+        return this._createSetCommand(uri, type, resource, to);
+    }
+
+    createDeleteCommand(uri, to) {
+        return this._createDeleteCommand(uri, to);
+    }
+
+    createMergeCommand(uri, type, resource, to) {
+        return this._createMergeCommand(uri, type, resource, to);
+    }
+
+    buildResourceQuery(uri, query) {
+        return this._buildResourceQuery(uri, query);
+    }
+
+    buildUri(uri, ...args) {
+        return this._buildUri(uri, ...args);
+    }
+
+    processCommand(command) {
+        return this._processCommand(command);
+    }
+
+}
