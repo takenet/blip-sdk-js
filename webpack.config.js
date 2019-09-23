3d360b8a0315bb81c6a6facd049888cc410cddf5
Henrique Torquato <henrique.ctorquato@gmail.com>
2019-09-23 17:13:32 -0300

    Added extension types

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
 module.exports = {
-    context: __dirname + '/src',
-    entry: './BlipSdk.js',
-    externals: {
-        'lime-js': {
-            root: 'Lime',
-            commonjs2: 'lime-js',
-            commonjs: 'lime-js',
-            amd: 'Lime'
-        },
-        'lime-transport-websocket': {
-            root: 'WebSocketTransport',
-            commonjs2: 'lime-transport-websocket',
-            commonjs: 'lime-transport-websocket',
-            amd: 'WebSocketTransport'
-        },
-        'bluebird': {
-            root: 'Promise',
-            commonjs2: 'bluebird',
-            commonjs: 'bluebird',
-            amd: 'Promise'
-        }
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: 'blip-sdk.js',
-        library: 'BlipSdk',
-        libraryTarget: 'umd'
-    },
-    module: {
-        preLoaders: [
-            { test: /(src|test)(.+)\.js$/, loader: 'eslint' }
-        ],
-        loaders: [
-            { test: /(src|test)(.+)\.js$/, exclude: /node_modules/, loader: 'babel' },
-            { test: /\.json$/, loader: 'json' }
-        ]
-    },
-    devtool: 'source-map'
+	context: __dirname + '/src',
+	entry: {
+		'blip-sdk.js': './BlipSdk.js',
+		'blip-sdk.d.ts': './Types.d.ts'
+	},
+	externals: {
+		'lime-js': {
+			root: 'Lime',
+			commonjs2: 'lime-js',
+			commonjs: 'lime-js',
+			amd: 'Lime'
+		},
+		'lime-transport-websocket': {
+			root: 'WebSocketTransport',
+			commonjs2: 'lime-transport-websocket',
+			commonjs: 'lime-transport-websocket',
+			amd: 'WebSocketTransport'
+		},
+		'bluebird': {
+			root: 'Promise',
+			commonjs2: 'bluebird',
+			commonjs: 'bluebird',
+			amd: 'Promise'
+		}
+	},
+	output: {
+		path: __dirname + '/dist',
+		filename: '[name]',
+		library: 'BlipSdk',
+		libraryTarget: 'umd'
+	},
+	module: {
+		preLoaders: [
+			{ test: /(src|test)(.+)\.js$/, loader: 'eslint' }
+		],
+		loaders: [
+			{ test: /(src|test)(.+)\.js$/, exclude: /node_modules/, loader: 'babel' },
+			{ test: /\.json$/, loader: 'json' },
+			{ test: /\.d.ts$/, exclude: /node_modules/, loader: 'raw-loader' },
+			{ test: /(d!)\.ts(x?)$/, exclude: /node_modules/, loader: 'ts-loader' },
+		]
+	},
+	devtool: 'source-map'
 };
